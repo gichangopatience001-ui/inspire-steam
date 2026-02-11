3a20c7cc0e554a6e404a9b948e36a94be6d4a33e
Patience Gichango <gichangopatience001@gmail.com>
2026-02-11 09:18:46 +0300

    first commit

---
+++ python-basics/challenge 1.py
@@ -0,0 +1,9 @@
+university = "tetr college of business"
+grade = "A"  
+dream_course = "Software Engineering"
+cluster_points = 84
+
+print(f"My dream university is {university}")
+print(f"I got an {grade} plain")
+print(f"My dream course is {dream_course}")
+print(f"i also have {cluster_points} points")
---
+++ python-basics/hello.py
@@ -0,0 +1,14 @@
+# this is a comment
+print("Hello from Patience")
+# city/town
+print ("I am from Nyahururu")
+# my hobbies
+print("I like cooking, paintig,singing, swimming and watching K-drama")
+
+age = 21
+height = 1.25
+weight = 75
+
+print(f"I am {age} years old ")
+print(f"I am also {height} tall")
+print(f"And weigh {weight} kgs")
