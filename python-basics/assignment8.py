90d1700401be85e46108d683eac716816b76cca6
Patience Gichango <gichangopatience001@gmail.com>
2026-02-23 08:54:14 +0300

    assignment8

--- python-basics/assignment8.py
+++ python-basics/assignment8.py
@@ -17,26 +17,26 @@
 # In both cases use the report_status methods to verify of damages are done
 
 class FighterCharacter:  
-     def init (self, role, health, damage, speed):
+     def __init__ (self, role, health, damage, speed):
         self.character_role = role
         self.character_health = health
         self.character_damage = damage
         self.character_speed = speed
 
-def run(self, direction):
+     def run(self, direction):
         print(f"Game log: {self.character_role} runs {direction} at speed {self.character_speed}")
 
-def report_status(self):
-        print(f"\nCharacter Log: \n Role: {self.character_role} \n Health: {self.character_health} \n Damage: {self.character_damage} \n Speed: {self.characterspeed}\n __ \n")
+     def report_status(self):
+        print(f"\nCharacter Log: \n Role: {self.character_role} \n Health: {self.character_health} \n Damage: {self.character_damage} \n Speed: {self.character_speed}\n __ \n")
 
-def kick(self, opponent):
+     def kick(self, opponent):
         character_damage = self.character_damage
         opponent.character_health = opponent.character_health - character_damage
         print(f"Game Log: {self.character_role} deals a damage of {character_damage} to the {opponent.character_role}.")
 
-def takle(self, opponent):
-      opponent.character_speed -= self.character_damage
-      print(f"Game Log: {self.character_role} tackles {opponent.character_role}, reducing their speed by {self.character_damage}!")
+     def takle(self, opponent):
+       opponent.character_speed -= self.character_damage
+       print(f"Game Log: {self.character_role} tackles {opponent.character_role}, reducing their speed by {self.character_damage}!")
         
 
 # Create FighterCharacter    
@@ -57,7 +57,7 @@ ninja_character.run("Up")
 ninja_character.kick(warrior_character)
 
 #NEW CODE FOR TACKLE
-warrior_character.tackle(ninja_character)
+warrior_character.takle(ninja_character)
 
 ninja_character.report_status()
 warrior_character.report_status()
